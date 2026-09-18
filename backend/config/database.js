4dc694f1378a452bb5b777bc2658eb47870fcc70
Aluno <e023aluno@outlook.com>
2026-09-18 16:01:37 -0300

    BACKEND: corrreçâo de erro no MmongoDB

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 
 async function conectarBanco() {
  try {
-    await mongoose.connect(ProcessingInstruction.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB local conectado com sucesso!');
  } catch (erro) {
     console.error('Erro ao conectar com o MOngoDB local:');
     console.error(erro.message);
-    ProcessingInstruction.exit(1);
+    process.exit(1);
  }
 }
 
